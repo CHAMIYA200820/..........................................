96fa8d4d94f74f8123def1d5e233b3ab4f614d49
CHAMIYA200820 <chamindumax33333@gmail.com>
2025-03-05 10:35:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DnhwCARa#4l6RRqaPriyJRd6qEK-avG4Y-s8Xu1jeOrnW87KY2EI", // Add your session id
-PREFIX: process.env.PREFIX || "#",
+SESSION_ID: process.env.SESSION_ID || "PINK-QUEEN-MD-o0YSHQAT#H6wiFPLZNL4S26R5U7A48j7mB6MB6v2-o08L2WemytQ", // Add your session id
+PREFIX: process.env.PREFIX || ".",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
